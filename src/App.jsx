7bcf5de411a007d8523d81d704a2bacb5e4018c5
Guilherme Padial <guilherme.padial4@gmail.com>
2025-03-17 11:54:28 -0300

    add: creation herobanner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Header } from './components/header/Header';
+import { HeroBanner } from './components/herobanner/HeroBanner';
+
 import './styles/_globals.scss';
 
 export function App() {
   return (
     <>
       <Header />
+      <HeroBanner />
     </>
   );
 }
---
+++ src/components/herobanner/HeroBanner.jsx
@@ -0,0 +1,17 @@
+import Banner from '../../assets/build.jpg';
+
+import './herobanner.scss';
+
+export function HeroBanner() {
+  return (
+    <section className="hero-banner" style={{ backgroundImage: `url(${Banner})` }}>
+      <div className="overlay">
+        <div className="content">
+          <h1>Construindo o Futuro com Qualidade e Confiança</h1>
+          <p>Na Dux Construtora, cada projeto é uma obra-prima que reflete excelência e dedicação.</p>
+          <button className="cta-btn">Conheça Nossos Projetos</button>
+        </div>
+      </div>
+    </section>
+  );
+}
