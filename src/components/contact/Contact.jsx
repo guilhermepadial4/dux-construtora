477de9d5e8a3f7048982cb602a78f51a6574d656
Guilherme Padial <guilherme.padial4@gmail.com>
2025-03-20 09:36:14 -0300

    update: contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,19 +11,56 @@ export function Contact() {
     message: '',
   });
   const [successMessage, setSuccessMessage] = useState(null);
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  });
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+    setErrors({ ...errors, [e.target.name]: '' });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
-    setFormData({ name: '', email: '', phone: '', message: '' });
-    setSuccessMessage('Sua mensagem foi enviada com sucesso!');
-    setTimeout(() => {
-      setSuccessMessage(null);
-    }, 3000);
+    let formIsValid = true;
+    const newErrors = { name: '', email: '', phone: '', message: '' };
+
+    if (!formData.name.trim()) {
+      newErrors.name = 'Por favor, insira seu nome.';
+      formIsValid = false;
+    }
+    if (!formData.email.trim()) {
+      newErrors.email = 'Por favor, insira seu e-mail.';
+      formIsValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = 'Por favor, insira um e-mail válido.';
+      formIsValid = false;
+    }
+    if (!formData.phone.trim()) {
+      newErrors.phone = 'Por favor, insira seu telefone.';
+      formIsValid = false;
+    } else if (!/^\d{10,11}$/.test(formData.phone)) {
+      newErrors.phone = 'Por favor, insira um telefone válido com 10 ou 11 dígitos.';
+      formIsValid = false;
+    }
+    if (!formData.message.trim()) {
+      newErrors.message = 'Por favor, insira sua mensagem.';
+      formIsValid = false;
+    }
+
+    setErrors(newErrors);
+
+    if (formIsValid) {
+      console.log(formData);
+      setFormData({ name: '', email: '', phone: '', message: '' });
+      setSuccessMessage('Sua mensagem foi enviada com sucesso!');
+      setTimeout(() => {
+        setSuccessMessage(null);
+      }, 3000);
+    }
   };
 
   return (
@@ -34,15 +71,29 @@ export function Contact() {
           <p>Estamos prontos para atender você!</p>
           <div className="info-item">
             <FaMapMarkerAlt className="icon" />
-            <p>Rua Vieira de Morais, 2110 - Campo Belo, SP</p>
+            <a
+              href="https://www.google.com/maps/place/R.+Vieira+de+Morais,+2110+-+Campo+Belo,+S%C3%A3o+Paulo+-+SP,+04617-007/@-23.6278539,-46.6659538,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce5a7ba7fe5405:0xcfd89b7bd4c30849!8m2!3d-23.6278588!4d-46.6633789!16s%2Fg%2F11b8z78319?entry=ttu&g_ep=EgoyMDI1MDMxOC4wIKXMDSoASAFQAw%3D%3D"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p>Rua Vieira de Morais, 2110 - Campo Belo, SP</p>
+            </a>
           </div>
           <div className="info-item">
             <FaPhoneAlt className="icon" />
-            <p>Telefone: (11) 96021-0555</p>
+            <a
+              href="https://api.whatsapp.com/send/?phone=%2B5511960210555&text&type=phone_number&app_absent=0"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <p>Telefone: (11) 96021-0555</p>
+            </a>
           </div>
           <div className="info-item">
             <FaEnvelope className="icon" />
-            <p>E-mail: contato@duxconstrutora.com.br</p>
+            <a href="mailto:contato@duxconstrutora.com.br">
+              <p>E-mail: contato@duxconstrutora.com.br</p>
+            </a>
           </div>
         </div>
         <div className="contact-form">
@@ -55,9 +106,13 @@ export function Contact() {
           )}
           <form onSubmit={handleSubmit}>
             <input type="text" name="name" placeholder="Nome" value={formData.name} onChange={handleChange} />
+            {errors.name && <p className="error-message">{errors.name}</p>}
             <input type="email" name="email" placeholder="E-mail" value={formData.email} onChange={handleChange} />
+            {errors.email && <p className="error-message">{errors.email}</p>}
             <input type="tel" name="phone" placeholder="Telefone" value={formData.phone} onChange={handleChange} />
+            {errors.phone && <p className="error-message">{errors.phone}</p>}
             <textarea name="message" placeholder="Mensagem" value={formData.message} onChange={handleChange} />
+            {errors.message && <p className="error-message">{errors.message}</p>}
             <button type="submit">Enviar</button>
           </form>
         </div>
