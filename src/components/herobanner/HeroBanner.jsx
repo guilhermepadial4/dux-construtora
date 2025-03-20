f0e8f85feaef8a078cc2280b402a846010e71b03
Guilherme Padial <guilherme.padial4@gmail.com>
2025-03-20 09:58:53 -0300

    add: link in button

--- src/components/herobanner/HeroBanner.jsx
+++ src/components/herobanner/HeroBanner.jsx
@@ -9,7 +9,9 @@ export function HeroBanner() {
         <div className="content">
           <h1>Construindo o Futuro com Qualidade e Confiança</h1>
           <p>Na Dux Construtora, cada projeto é uma obra-prima que reflete excelência e dedicação.</p>
-          <button className="cta-btn">Conheça Nossos Projetos</button>
+          <a href="https://duxconstrutora.com.br/projetos/" target="_blank" rel="noopener noreferrer">
+            <button className="cta-btn">Conheça Nossos Projetos</button>
+          </a>
         </div>
       </div>
     </section>
